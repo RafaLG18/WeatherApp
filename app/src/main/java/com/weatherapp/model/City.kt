682af112a6f76027e5482b8ae8dd7bfd3494a337
rafaLG18 <rafaellealg18@gmail.com>
2026-06-03 23:33:43 -0300

    #14: Pratica6-parte-4

--- app/src/main/java/com/weatherapp/model/City.kt
+++ app/src/main/java/com/weatherapp/model/City.kt
@@ -1,9 +1,18 @@
 package com.weatherapp.model
 
 import com.google.android.gms.maps.model.LatLng
+import com.weatherapp.db.fb.FBCity
 
 data class City (
     val name : String,
     val weather: String? = null,
     val location: LatLng? = null
-)
+) {
+    fun toFBCity(): FBCity {
+        val fbCity = FBCity()
+        fbCity.name = this.name
+        fbCity.lat = this.location?.latitude ?: 0.0
+        fbCity.lng = this.location?.longitude ?: 0.0
+        return fbCity
+    }
+}
