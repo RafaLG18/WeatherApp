88ef92ea6f7013a6745d5eb3d876ee10b3cbf02b
rafaLG18 <rafaellealg18@gmail.com>
2026-06-22 20:52:41 -0300

    #20: pratica-07-parte-02

--- app/src/main/java/com/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/weatherapp/MainViewModel.kt
@@ -5,54 +5,78 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.model.LatLng
+import com.weatherapp.api.WeatherService
 import com.weatherapp.db.fb.FBCity
 import com.weatherapp.db.fb.FBDatabase
 import com.weatherapp.db.fb.FBUser
 import com.weatherapp.model.City
 import com.weatherapp.model.User
 
-class MainViewModel (private val db: FBDatabase): ViewModel(),
+class MainViewModel (private val db: FBDatabase, private val service : WeatherService): ViewModel(),
     FBDatabase.Listener {
     private val _cities = mutableStateListOf<City>()
     val cities
         get() = _cities.toList()
-    private val _user = mutableStateOf<User?> (null)
-    val user : User?
+    private val _user = mutableStateOf<User?>(null)
+    val user: User?
         get() = _user.value
+
     init {
         db.setListener(this)
     }
+
     fun remove(city: City) {
         db.remove(city.toFBCity())
     }
-    fun add(name: String, location : LatLng? = null) {
+
+    fun add(name: String, location: LatLng? = null) {
         db.add(City(name = name, location = location).toFBCity())
     }
+
     override fun onUserLoaded(user: FBUser) {
         _user.value = user.toUser()
     }
+
     override fun onUserSignOut() {
         //TODO("Not yet implemented")
     }
+
     override fun onCityAdded(city: FBCity) {
         _cities.add(city.toCity())
     }
+
     override fun onCityUpdated(city: FBCity) {
         //TODO("Not yet implemented")
     }
+
     override fun onCityRemoved(city: FBCity) {
         _cities.remove(city.toCity())
     }
-}
-class MainViewModelFactory(private val db : FBDatabase) :
-    ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
-            return MainViewModel(db) as T
+
+    fun addCity(name: String) {
+        service.getLocation(name) { lat, lng ->
+            if (lat != null && lng != null) {
+                db.add(City(name = name, location = LatLng(lat, lng)).toFBCity())
+            }
         }
-        throw IllegalArgumentException("Unknown ViewModel class")
     }
-}
-private fun getCities() = List(20) { i ->
-    City(name = "Cidade $i", weather = "Carregando clima...")
+
+    fun addCity(location: LatLng) {
+        service.getName(location.latitude, location.longitude) { name ->
+            if (name != null) {
+                db.add(City(name = name, location = location).toFBCity())
+            }
+        }
+
+    }
+
+    class MainViewModelFactory(private val db: FBDatabase, private val service: WeatherService) :
+        ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+                return MainViewModel(db, service) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
 }
